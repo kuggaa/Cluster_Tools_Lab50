62c712b00f80352d8aa625c4c775291db22745cc
Georgiy Sgibnev <user@SgibnevGY@mail50.stend>
2014-03-24 11:49:21 +0400

    Exception ProcessError.

--- cluster_tools/exceptions.py
+++ cluster_tools/exceptions.py
@@ -1,3 +1,9 @@
 class DeviceError(Exception):
     def __init__(self, device_id):
         self.device_id = device_id
+
+
+class ProcessError(Exception):
+    def __init__(self, args, err_output):
+        self.cmd = " ".join(args)
+        self.err_output = err_output
